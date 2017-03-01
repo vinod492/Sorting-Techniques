629e53340f09fd2540e3baefbf1a1fc096c853ae
Vinod Reddy <vinod492@gmail.com>
2017-02-28 21:07:06 -0700

    Add comments

--- bubblesort/bubblesort.js
+++ bubblesort/bubblesort.js
@@ -15,24 +15,33 @@ function bubbleSort() {
        console.log(array);
    }
 
-   function bubbleSort1() {
+/* Bubble Sort*/
+/* 
+arr = 5,4,8,1;
+i=0                                                    i=1                                                       i=2
+It1: 4,5,8,1 (compare 0,1 elements)                    It1: 4,5,1,8 (compare 0,1 elements)                       It1: 1,4,5,8 (compare 0,1 elements) arr[1] is set 
+It2: 4,5,8,1 (compare 1,2 elements)                    It2: 4,1,5,8 (compare 1,2 elements) arr[2] is set
+It3: 4,5,1,8 (compare 2,3 elements) arr[3] is set  
+*/ 
 
-       var array = [5, 4, 3, 2, 1];
-       
-       document.getElementById("array").value = array.toString();
-     
-       var len = array.length;
-       for (var i = 0; i < len; i++) {
-           for (var j = 0; j < (len-i-1); j++) {
-               if (array[j]>array[j+1]) {
-                   var temp = array[j];
-                   array[j] = array[j + 1];
-                   array[j + 1] = temp;
-               }
-           }
-       }
+function bubbleSort1() {
 
-       document.getElementById("sortedarray").value = array.toString();
-       
-   }
+    var array = [5, 4, 3, 2, 1];
+
+    document.getElementById("array").value = array.toString();
+
+    var len = array.length;
+    for (var i = 0; i < len; i++) {
+        for (var j = 0; j < (len - i - 1); j++) {
+            if (array[j] > array[j + 1]) {
+                var temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+
+    document.getElementById("sortedarray").value = array.toString();
+
+}
 
